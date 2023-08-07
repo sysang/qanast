afdfc43108a052cee59175546eeab24675d51fa8
sysang <daosysang@gmail.com>
2023-08-07 23:24:23 +0700

    Integrate Meilisearch

---
+++ add_document_meilisearch.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import json
+import re
+
+import meilisearch
+
+
+client = meilisearch.Client('http://192.168.58.9:7700')
+
+def is_json_file(fname):
+  is_jlext_p = re.compile('\.json$')
+  return is_jlext_p.search(fname) is not None
+
+path_obj = Path('my-crawler/storage/datasets/default')
+data = []
+for file in path_obj.iterdir():
+    if not is_json_file(file.name):
+        continue
+
+    with open(file.absolute()) as json_file:
+        doc = json.load(json_file)
+        doc['id'] = doc['hashed']
+        data.append(doc)
+
+client.index('leteemartdalat').add_documents(data)
