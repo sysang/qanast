acb3042607b12c8a3b3d9499b2782e0e5569cbb8
sysang <daosysang@gmail.com>
2023-10-28 21:01:38 +0700

    Complete unit tests for bot mechansim

--- src/botframework/dialogs/root-dialog.spec.ts
+++ src/botframework/dialogs/root-dialog.spec.ts
@@ -1,63 +1,79 @@
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 
-import { type BotState } from 'botbuilder-core';
+import { MemoryStorage } from 'botbuilder';
+import { TestAdapter } from 'botbuilder-core';
 import {
+  ComponentDialog,
+  TextPrompt,
   WaterfallDialog,
   type WaterfallStepContext
 } from 'botbuilder-dialogs';
 
-import { DialogTestClient } from 'botbuilder-testing';
-import { MemoryStorage, UserState, ConversationState } from 'botbuilder';
+import { DialogTestClient } from '../test-helpers/dialog-test-client';
+import createBot from '../bots/bot';
 
-import DialogueManager from '../bots/dialogue-manager';
-import MyComponentDialog from './my-dialogs';
-import RootDialog from './root-dialog';
-
+const TEXT_PROMPT = 'TEXT_PROMPT';
 const WATERFALL_DIALOG = 'WATERFALL_DIALOG';
 const SIMPLE_DIALOG_ID = 'SIMPLE_DIALOG_ID'
+const SIMPLE_DIALOG_STEP_MESSAGE = 'test message';
 const SIMPLE_DIALOG_STEP_RESULT = 'test tokens';
 
-export class SimpleDialog extends MyComponentDialog {
-  constructor (userState: BotState) {
+export class SimpleDialog extends ComponentDialog {
+  constructor () {
     super(SIMPLE_DIALOG_ID);
 
+    this.addDialog(new TextPrompt(TEXT_PROMPT));
     this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
-      this.stepOne.bind(this)
+      this.stepOne.bind(this),
+      this.stepTwo.bind(this)
     ]));
 
     this.initialDialogId = WATERFALL_DIALOG;
   }
 
   async stepOne (stepContext: WaterfallStepContext) {
+    return await stepContext.beginDialog(TEXT_PROMPT, { prompt: SIMPLE_DIALOG_STEP_MESSAGE });
+  }
+
+  async stepTwo (stepContext: WaterfallStepContext) {
     return await stepContext.endDialog(SIMPLE_DIALOG_STEP_RESULT);
   }
 }
 
 function initilizeClient () {
   const storage = new MemoryStorage();
-  const userState = new UserState(storage);
-  const conversationState = new ConversationState(storage);
-  const simpleDialog = new SimpleDialog(userState);
-  const dialogueManager = new DialogueManager([ simpleDialog ]);
-  const rootDialog = new RootDialog(conversationState, userState, dialogueManager);
-  const client =  new DialogTestClient('test', rootDialog);
+  const dialogs = [new SimpleDialog()];
+  const dialogBot = createBot(storage, dialogs);
 
-  return { client, dialogueManager, rootDialog, simpleDialog }
+  const adapter = new TestAdapter(async (context): Promise<void> => { await dialogBot.run(context) });
+
+  const client = new DialogTestClient(adapter);
+
+  return { client, adapter, dialogBot, dialogs }
 }
 
-describe('RootDialog', () => {
-  // test('Reply result returned from child dialog', async () => {
-  //   const { client, dialogueManager, rootDialog, simpleDialog } = initilizeClient(); 
-  //   const reply = await client.sendActivity('hello');
-  //   expect(reply.text).toBe(SIMPLE_DIALOG_STEP_RESULT);
-  // });
+describe('DialogBot + DialogueManager + RootDialog', () => {
+  test('Should get reply as TextPrompt and should get reply as SimpleDialog result', async () => {
+    const { client } = initilizeClient();
+    let reply = await client.sendActivity('hello');
+    expect(reply.text).toBe(SIMPLE_DIALOG_STEP_MESSAGE);
+    reply = await client.sendActivity('test');
+    expect(reply.text).toBe(SIMPLE_DIALOG_STEP_RESULT);
+  });
 
-  test('Correct history after responsing to first user\'s message', async () => {
-    const { client, dialogueManager, rootDialog, simpleDialog } = initilizeClient(); 
+  test('Should have correct event history', async () => {
+    const { client, dialogBot } = initilizeClient();
     await client.sendActivity('hello');
-    await client.sendActivity('goodbye');
-    const history = await rootDialog.getHistory();
-    console.log(history);
+    await client.sendActivity('test');
+    const history = dialogBot.rootDialog.dialogs.getHistory();
+
+    const actual = {
+      '0': { role: 'user', text: 'hello' },
+      '1': { role: 'bot', text: SIMPLE_DIALOG_STEP_MESSAGE},
+      '2': { role: 'user', text: 'test' },
+      '3': { role: 'bot', text: SIMPLE_DIALOG_STEP_RESULT }
+    }
     expect(Object.keys(history).length).toBe(4);
+    expect(history).toStrictEqual(actual);
   });
 });
